86ed7893e58ff05a7fc51fc3cd5a6cd347578a9a
Tranhuutukkt <tranhuutukkt@gmail.com>
2023-02-27 14:54:30 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://tranhuutukk.github.io/149nan/"
+  base: "/149nan/"
 })
